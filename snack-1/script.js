2b812a5e0d533699fa1d6214901814cd19da9628
mattiacoli <mattia.coli@aol.com>
2025-02-26 13:26:23 +0100

    add snack1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,9 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+
+
+names.forEach((name)=>{
+  console.log(name);
+})
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
